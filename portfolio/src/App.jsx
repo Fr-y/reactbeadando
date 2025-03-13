f319f0bd46f7a7c0a7c3773e32f242f545d086d6
Fr-y <gaborkorpa@gmail.com>
2025-03-13 12:01:04 +0100

    Navbar and base components

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,7 +1,10 @@
+import React from 'react';
+import Home from './components/Home';
+
 export default function App() {
   return (
-    <>
-      
-    </>
+    <div className='App container'>
+      <Home />
+    </div>
   )
 }
---
+++ portfolio/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+    return (
+      <header>
+        <a href="">Works</a>
+        <a href="">Blog</a>
+        <a href="">Contact</a>
+      </header>
+    )
+  }
